0e90421fc2bbcd81c0961b28e52de6f21d09ccd3
devindt99 <devindt99@gmail.com>
2024-03-11 16:37:48 -0700

    Added front-end (multipage) routing

---
+++ mountain-beaver-client/src/pages/InventoryPage/InventoryPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const InventoryPage = () => {
+
+  return (
+    <div>
+      <h1>INVENTORY</h1>
+
+    </div>
+  );
+};
+
+export default InventoryPage;
---
+++ mountain-beaver-client/src/pages/WarehousesPage/WarehousesPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const WarehousesPage = () => {
+
+  return (
+    <div>
+      <h1>WAREHOUSES</h1>
+
+    </div>
+  );
+};
+
+export default WarehousesPage;
