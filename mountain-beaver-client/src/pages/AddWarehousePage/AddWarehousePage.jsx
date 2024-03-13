dd107540c9d6eff76b95d149c53e5f54e9a9deaf
Sowmya Malur <smalur.github@gmail.com>
2024-03-13 09:51:09 -0700

    Add validation for the form fields warehousename, streetaddress, city

--- mountain-beaver-client/src/pages/AddWarehousePage/AddWarehousePage.jsx
+++ mountain-beaver-client/src/pages/AddWarehousePage/AddWarehousePage.jsx
@@ -1,10 +1,100 @@
 import backarrow from "../../assets/icons/arrow_back-24px.svg";
+import erroricon from "../../assets/icons/error-24px.svg";
+
 import "../AddWarehousePage/AddWarehousePage.scss";
 
 // import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useState, useRef } from "react";
 
 function AddWarehousePage() {
+  const errorMessage = "This field is required";
+
+  // Set use states for all the fields
+  const [wareHouseName, setWareHouseName] = useState("");
+  const [streetAddress, setStreetAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [contactName, setContactName] = useState("");
+  const [position, setPosition] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [errors, setErrors] = useState({});
+
+  // Set refs for all the fields
+  const formRef = useRef();
+  const nameRef = useRef();
+  const addressRef = useRef();
+  const cityRef = useRef();
+  const countryRef = useRef();
+  const contactRef = useRef();
+  const positionRef = useRef();
+  const phoneRef = useRef();
+  const emailRef = useRef();
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === "wareHouseName") {
+      setWareHouseName(value);
+    } else if (name === "streetAddress") {
+      setStreetAddress(value);
+    } else if (name === "city") {
+      setCity(value);
+    } else if (name === "country") {
+      setCountry(value);
+    } else if (name === "contactName") {
+      setContactName(value);
+    } else if (name === "position") {
+      setPosition(value);
+    } else if (name === "phoneNumber") {
+      setPhoneNumber(value);
+    } else {
+      setEmail(value);
+    }
+
+    // Clear errors and hide error message when the user enters a value
+    if (value.trim() !== "") {
+      setErrors({});
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("in submit function");
+
+    const form = formRef.current;
+
+    let formErrors = {};
+
+    // get the values from the form
+    const wareHouseName = form.wareHouseName.value;
+    const streetAddress = form.streetAddress.value;
+
+    if (wareHouseName.trim() === "") {
+      nameRef.current.focus();
+      formErrors.wareHouseName = errorMessage;
+    }
+    if (streetAddress.trim() === "") {
+      addressRef.current.focus();
+      formErrors.streetAddress = errorMessage;
+    }
+    if (city.trim() === "") {
+      cityRef.current.focus();
+      formErrors.city = errorMessage;
+    }
+
+    console.log(formErrors);
+    if (formErrors.length !== 0) {
+      setErrors(formErrors);
+    }
+
+    if (Object.keys(formErrors).length === 0) {
+      // call axios to post data here
+      console.log("New warehouse added successfully");
+    }
+  };
+
   return (
     <section className="add-warehouse">
       <div className="add-warehouse__page-title">
@@ -14,20 +104,35 @@ function AddWarehousePage() {
         <h1 className="add-warehouse__title">Add New Warehouse</h1>
       </div>
 
-      <form className="add-warehouse__form">
+      <form
+        className="add-warehouse__form"
+        id="add-warehouse-form"
+        ref={formRef}
+      >
         <div className="two-column-container">
           <div className="add-warehouse__details">
             <h2 className="add-warehouse__sub-title">Warehouse Details</h2>
-            <label htmlFor="warehouseName" className="add-warehouse__label">
+            <label htmlFor="wareHouseName" className="add-warehouse__label">
               Warehouse Name
             </label>
             <input
               type="text"
-              name="warehouseName"
-              id="warehouseName"
+              name="wareHouseName"
+              id="wareHouseName"
               placeholder="Warehouse Name"
-              className="add-warehouse__field"
+              className={`add-warehouse__field ${
+                errors.wareHouseName && "add-warehouse__field--error"
+              }`}
+              value={wareHouseName}
+              onChange={handleChange}
+              ref={nameRef}
             />
+            {errors.wareHouseName && (
+              <div className="add-warehouse__error-message">
+                <img src={erroricon} alt="error icon"/>
+                {errors.wareHouseName}
+              </div>
+            )}
             <label htmlFor="streetAddress" className="add-warehouse__label">
               Street Address
             </label>
@@ -36,8 +141,20 @@ function AddWarehousePage() {
               name="streetAddress"
               id="streetAddress"
               placeholder="Street Address"
-              className="add-warehouse__field"
+              className={`add-warehouse__field ${
+                errors.streetAddress && "add-warehouse__field--error"
+              }`}
+              value={streetAddress}
+              onChange={handleChange}
+              ref={addressRef}
             />
+            {errors.streetAddress && (
+              <div className="add-warehouse__error-message">
+                <img src={erroricon} alt="error icon"/>
+                {errors.streetAddress}
+              </div>
+            )}
+
             <label htmlFor="city" className="add-warehouse__label">
               City
             </label>
@@ -46,8 +163,19 @@ function AddWarehousePage() {
               name="city"
               id="city"
               placeholder="City"
-              className="add-warehouse__field"
+              className={`add-warehouse__field ${
+                errors.city && "add-warehouse__field--error"
+              }`}
+              value={city}
+              onChange={handleChange}
+              ref={cityRef}
             />
+            {errors.city && (
+              <div className="add-warehouse__error-message">
+                <img src={erroricon} alt="error icon"/>
+                {errors.city}
+              </div>
+            )}
             <label htmlFor="country" className="add-warehouse__label">
               Country
             </label>
@@ -109,11 +237,15 @@ function AddWarehousePage() {
               Cancel
             </button>
           </Link>
-          <Link to="/">
-            <button id="addWarehouse" className="add-warehouse__cta">
-              + Add Warehouse
-            </button>
-          </Link>
+          {/* <Link to="/"> */}
+          <button
+            id="addWarehouse"
+            className="add-warehouse__cta"
+            onClick={handleSubmit}
+          >
+            + Add Warehouse
+          </button>
+          {/* </Link> */}
         </div>
       </form>
     </section>
