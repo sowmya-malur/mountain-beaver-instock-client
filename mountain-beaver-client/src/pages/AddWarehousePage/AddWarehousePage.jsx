923111ff0fe17f9bb8a1fb1981ea12bca932f985
Sowmya Malur <smalur.github@gmail.com>
2024-03-12 22:41:37 -0700

    Clean up unnecessary code and format

--- mountain-beaver-client/src/pages/AddWarehousePage/AddWarehousePage.jsx
+++ mountain-beaver-client/src/pages/AddWarehousePage/AddWarehousePage.jsx
@@ -13,67 +13,95 @@ function AddWarehousePage() {
         </Link>
         <h1 className="add-warehouse__title">Add New Warehouse</h1>
       </div>
-      
+
       <form className="add-warehouse__form">
-      <div className="two-column-container">
-        <div className="add-warehouse__details">
-          <h2 className="add-warehouse__sub-title">Warehouse Details</h2>
-          <label htmlFor="warehouseName" className="add-warehouse__label">Warehouse Name</label>
-          <input
-            type="text"
-            name="warehouseName"
-            id="warehouseName"
-            placeholder="Warehouse Name"
-            className="add-warehouse__field"
-          />
-          <label htmlFor="streetAddress" className="add-warehouse__label">Street Address</label>
-          <input
-            type="text"
-            name="streetAddress"
-            id="streetAddress"
-            placeholder="Street Address"
-            className="add-warehouse__field"
-          />
-          <label htmlFor="city" className="add-warehouse__label">City</label>
-          <input type="text" name="city" id="city" placeholder="City" className="add-warehouse__field"/>
-          <label htmlFor="country" className="add-warehouse__label">Country</label>
-          <input
-            type="text"
-            name="country"
-            id="country"
-            placeholder="Country"
-            className="add-warehouse__field"
-          />
-        </div>
-        <div className="add-warehouse__contact">
-          <h2 className="add-warehouse__sub-title">Contact Details</h2>
-          <label htmlFor="contactName" className="add-warehouse__label">Contact Name</label>
-          <input
-            type="text"
-            name="contactName"
-            id="contactName"
-            placeholder="Contact Name"
-            className="add-warehouse__field"
-          />
-          <label htmlFor="position" className="add-warehouse__label">Position</label>
-          <input
-            type="text"
-            name="position"
-            id="position"
-            placeholder="Position"
-            className="add-warehouse__field"
-          />
-          <label htmlFor="phoneNumber" className="add-warehouse__label">Phone Number</label>
-          <input
-            type="text"
-            name="phoneNumber"
-            id="phoneNumber"
-            placeholder="Phone Number"
-            className="add-warehouse__field"
-          />
-          <label htmlFor="email" className="add-warehouse__label">Email</label>
-          <input type="text" name="email" id="email" placeholder="Email" className="add-warehouse__field"/>
-        </div>
+        <div className="two-column-container">
+          <div className="add-warehouse__details">
+            <h2 className="add-warehouse__sub-title">Warehouse Details</h2>
+            <label htmlFor="warehouseName" className="add-warehouse__label">
+              Warehouse Name
+            </label>
+            <input
+              type="text"
+              name="warehouseName"
+              id="warehouseName"
+              placeholder="Warehouse Name"
+              className="add-warehouse__field"
+            />
+            <label htmlFor="streetAddress" className="add-warehouse__label">
+              Street Address
+            </label>
+            <input
+              type="text"
+              name="streetAddress"
+              id="streetAddress"
+              placeholder="Street Address"
+              className="add-warehouse__field"
+            />
+            <label htmlFor="city" className="add-warehouse__label">
+              City
+            </label>
+            <input
+              type="text"
+              name="city"
+              id="city"
+              placeholder="City"
+              className="add-warehouse__field"
+            />
+            <label htmlFor="country" className="add-warehouse__label">
+              Country
+            </label>
+            <input
+              type="text"
+              name="country"
+              id="country"
+              placeholder="Country"
+              className="add-warehouse__field"
+            />
+          </div>
+          <div className="add-warehouse__contact">
+            <h2 className="add-warehouse__sub-title">Contact Details</h2>
+            <label htmlFor="contactName" className="add-warehouse__label">
+              Contact Name
+            </label>
+            <input
+              type="text"
+              name="contactName"
+              id="contactName"
+              placeholder="Contact Name"
+              className="add-warehouse__field"
+            />
+            <label htmlFor="position" className="add-warehouse__label">
+              Position
+            </label>
+            <input
+              type="text"
+              name="position"
+              id="position"
+              placeholder="Position"
+              className="add-warehouse__field"
+            />
+            <label htmlFor="phoneNumber" className="add-warehouse__label">
+              Phone Number
+            </label>
+            <input
+              type="text"
+              name="phoneNumber"
+              id="phoneNumber"
+              placeholder="Phone Number"
+              className="add-warehouse__field"
+            />
+            <label htmlFor="email" className="add-warehouse__label">
+              Email
+            </label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="Email"
+              className="add-warehouse__field"
+            />
+          </div>
         </div>
         <div className="add-warehouse__buttons-container">
           <Link to="/" className="add-warehouse__wrapper">
@@ -81,14 +109,13 @@ function AddWarehousePage() {
               Cancel
             </button>
           </Link>
-          <Link to="/" >
+          <Link to="/">
             <button id="addWarehouse" className="add-warehouse__cta">
-            + Add Warehouse
-          </button>
+              + Add Warehouse
+            </button>
           </Link>
         </div>
       </form>
-     
     </section>
   );
 }
