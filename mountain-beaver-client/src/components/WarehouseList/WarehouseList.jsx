c14c90e78bc19d463f3c91c0057636b81c875b49
devindt99 <devindt99@gmail.com>
2024-03-13 17:10:13 -0700

    cleanup commit to feature-J24MB-20

--- mountain-beaver-client/src/components/WarehouseList/WarehouseList.jsx
+++ mountain-beaver-client/src/components/WarehouseList/WarehouseList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-
+import './WarehouseList.scss';
 const WarehouseList = () => {
   const [warehouses, setWarehouses] = useState([]);
 
@@ -19,20 +19,17 @@ const WarehouseList = () => {
 
   return (
     <div>
-      <h2>Warehouses List</h2>
+      <h2 className='warehouse__heading'>Warehouses</h2>
+      <button className='warehouse__heading'></button>
       {warehouses.length > 0 ? (
-        <table>
+        <table className='warehouse__table'>
           <thead>
             <tr>
-              <th>Warehouse Name</th>
-              <th>Address</th>
-              <th>City</th>
-              <th>Country</th>
-              <th>Contact_name</th>
-              <th>Contact_position</th>
-              <th>Contact_phone</th>
-              <th>Contact_email</th>
-              
+              <th className='warehouse__cell'>Warehouse<div class="chevron"></div></th>
+              <th className='warehouse__cell'>Address<i class="chevron"></i></th>
+              <th className='warehouse__cell'>Contact Name<i class="chevron"></i></th>
+              <th className='warehouse__cell'> Contact information<i class="chevron"></i></th>
+              <th className='warehouse__cell'>Actions<i class="chevron"></i></th> 
             </tr>
           </thead>
           <tbody>
@@ -40,12 +37,9 @@ const WarehouseList = () => {
               <tr key={index}>
                 <td>{warehouse.warehouse_name}</td>
                 <td>{warehouse.address}</td>
-                <td>{warehouse.city}</td>
-                <td>{warehouse.country}</td>
                 <td>{warehouse.contact_name}</td>
-                <td>{warehouse.contact_position}</td>
-                <td>{warehouse.contact_phone}</td>
-                <td>{warehouse.contact_email}</td>
+                <td>{warehouse.contact_phone}
+                <br/>{warehouse.contact_email}</td>
                
               </tr>
             ))}
