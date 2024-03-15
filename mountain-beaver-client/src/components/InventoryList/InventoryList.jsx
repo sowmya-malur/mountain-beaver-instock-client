ad4dda5763dad93becae78cd992a35186f73b500
aalyosify <eale.kaber@gmail.com>
2024-03-15 15:51:54 -0700

    Added styling for the MoblieList component

--- mountain-beaver-client/src/components/InventoryList/InventoryList.jsx
+++ mountain-beaver-client/src/components/InventoryList/InventoryList.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import MobileList from "../MobileList/MobileList";
 import RegularList from "../RegularList/RegularList";
+import "./InventoryList.scss";
+import SearchLogo from "../../assets/icons/search-24px.svg";
 
 const InventoryList = () => {
   const [Inventories, setInventories] = useState([]);
@@ -10,7 +12,7 @@ const InventoryList = () => {
     const fetchInventories = async () => {
       try {
         const response = await axios.get(
-          `${process.env.REACT_APP_BACKEND_URL}/inventory/`
+          `${process.env.REACT_APP_BACKEND_URL}/api/inventories/`
         );
         setInventories(response.data);
       } catch (error) {
@@ -22,18 +24,45 @@ const InventoryList = () => {
   }, []);
 
   return (
-    <div>
-      <h2>Inventories List</h2>
-      <MobileList
-        list={Inventories}
-        titles={["INVENTORY ITEM", "CATEGORY", "STATUS", "QTY", "WAREHOUSE"]}
-      />
-      <RegularList
-        list={Inventories}
-        titles={["INVENTORY ITEM", "CATEGORY", "STATUS", "QTY", "WAREHOUSE", "ACTIONS"]}
-      />
+    <div className="inventory">
+      <div className="inventory__wrapper">
+        <div className="inventory__container">
+          <h2 className="inventory__title">Inventory</h2>
+          <div className="inventory__input-wrapper">
+            <input
+              type="text"
+              id="serach"
+              name="serach"
+              placeholder="Search..."
+              className="inventory__input"
+            />
+            <img src={SearchLogo} alt="" className="inventory__logo" />
+
+            <button className="inventory__button">+Add New Item</button>
+          </div>
+        </div>
+
+        <MobileList
+          list={Inventories}
+          titles={["INVENTORY ITEM", "CATEGORY", "STATUS", "QTY", "WAREHOUSE"]}
+        />
+      </div>
     </div>
   );
 };
 
 export default InventoryList;
+
+{
+  /* <RegularList
+        list={Inventories}
+        titles={[
+          "INVENTORY ITEM",
+          "CATEGORY",
+          "STATUS",
+          "QTY",
+          "WAREHOUSE",
+          "ACTIONS",
+        ]}
+      /> */
+}
