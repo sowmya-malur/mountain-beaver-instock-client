292718f120d6b5c74c7e6964201d39980dce3645
Sowmya Malur <smalur.github@gmail.com>
2024-03-16 12:38:08 -0700

    Fix errors after merge from develop

--- mountain-beaver-client/src/components/InventoryList/InventoryList.jsx
+++ mountain-beaver-client/src/components/InventoryList/InventoryList.jsx
@@ -53,36 +53,36 @@ const InventoryList = () => {
     <div>
       <h2>Inventories List</h2>
 
-//       {Inventories.length > 0 ? (
-//         <table>
-//   <thead>
-//     <tr>
-//       <th>Item Name</th> {/* Assuming you intended to display this */}
-//       <th>Description</th>
-//       <th>Category</th>
-//       <th>Status</th>
-//       <th>Quantity</th>
-//       <th>Created At</th>
-//       <th>Updated At</th>
-//     </tr>
-//   </thead>
-//   <tbody>
-//     {Inventories.map((inventory) => (
-//       <tr key={inventory.id}>
-//         <td>{inventory.item_name}</td> {/* Corrected to include Item Name */}
-//         <td>{inventory.description}</td>
-//         <td>{inventory.category}</td>
-//         <td>{inventory.status}</td>
-//         <td>{inventory.quantity}</td>
-//         <td>{new Date(inventory.created_at).toLocaleString()}</td>
-//         <td>{new Date(inventory.updated_at).toLocaleString()}</td>
-//       </tr>
-//     ))}
-//   </tbody>
-// </table>
-//       ) : (
-//         <p>No Inventories found.</p>
-//       )}
+       {Inventories.length > 0 ? (
+        <table>
+  <thead>
+    <tr>
+      <th>Item Name</th> {/* Assuming you intended to display this */}
+      <th>Description</th>
+      <th>Category</th>
+      <th>Status</th>
+      <th>Quantity</th>
+      <th>Created At</th>
+      <th>Updated At</th>
+    </tr>
+  </thead>
+  <tbody>
+    {Inventories.map((inventory) => (
+      <tr key={inventory.id}>
+        <td>{inventory.item_name}</td> {/* Corrected to include Item Name */}
+        <td>{inventory.description}</td>
+        <td>{inventory.category}</td>
+        <td>{inventory.status}</td>
+        <td>{inventory.quantity}</td>
+        <td>{new Date(inventory.created_at).toLocaleString()}</td>
+        <td>{new Date(inventory.updated_at).toLocaleString()}</td>
+      </tr>
+    ))}
+  </tbody>
+</table>
+      ) : (
+        <p>No Inventories found.</p>
+      )}
 
       <MobileList
         list={Inventories}
@@ -99,6 +99,7 @@ const InventoryList = () => {
           "ACTIONS",
         ]}
       /> */}
+    </div>
     </div>
   );
 };
